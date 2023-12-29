4135337c7ef87e20850434b1269b4c8bdc9ccfd6
Aslil <110744852+Aslil@users.noreply.github.com>
2023-12-29 14:47:58 +0300

    Museum.java

--- hw1/q2/Museum.java
+++ hw1/q2/Museum.java
@@ -1,7 +1,6 @@
 //-----------------------------------------------------
 // Title: Museum class
 // Author: Asli Algin
-// ID: 23275494084
 // Section: 02
 // Assignment: 1
 // Description: The Museum class defines a simple class which has a two instance variables.
